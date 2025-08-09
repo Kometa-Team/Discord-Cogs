41c3052a72f7d99f51de790ea4f6b35d10d82c71
bullmoose20 <12549033+bullmoose20@users.noreply.github.com.>
2025-08-09 10:59:50 -0400

    improvement to security message

--- logscan/logscan.py
+++ logscan/logscan.py
@@ -1526,6 +1526,7 @@ class RedBotCogLogscan(commands.Cog):
             vuln_low_str  = ".".join(map(str, _PMS_VULN_LOW))
             vuln_high_str = ".".join(map(str, _PMS_VULN_HIGH))
             url_line = "[https://forums.plex.tv/t/plex-media-server-security-update/928341]"
+            formatted_errors = self.format_contiguous_lines(security_vuln_hits)
 
             msg = (
                 "🚀 **PMS SECURITY ALERT**\n"
@@ -1533,6 +1534,7 @@ class RedBotCogLogscan(commands.Cog):
                 f"**Affected range:** `{vuln_low_str}` **through** `{vuln_high_str}`\n"
                 "Please **upgrade Plex Media Server** to a safe release as soon as possible.\n"
                 f"For more information on this see url: {url_line}\n"
+                f"{len(security_vuln_hits)} line(s) with these errors. Line number(s): {formatted_errors}"
                 "Detected on:\n"
             )
             for sn, ver, ln in items:
